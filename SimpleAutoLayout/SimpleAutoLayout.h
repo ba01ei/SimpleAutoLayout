3fb02f2d2c63d260d805c797736934be0d50032c
Bao Lei <bao.lei@me.com>
2016-04-09 23:41:39 -0700

    Initial Commit

---
+++ SimpleAutoLayout/SimpleAutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  SimpleAutoLayout.h
+//  SimpleAutoLayout
+//
+//  Created by Bao Lei on 4/9/16.
+//  Copyright © 2016 Bao Lei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleAutoLayout.
+FOUNDATION_EXPORT double SimpleAutoLayoutVersionNumber;
+
+//! Project version string for SimpleAutoLayout.
+FOUNDATION_EXPORT const unsigned char SimpleAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleAutoLayout/PublicHeader.h>
+
+
